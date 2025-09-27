d2400c2373a77b754fb9ee8d55f4a9972c864e00
s-afrid <afrid0052@gmail.com>
2025-09-27 17:05:10 +0530

    serving views

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import express from "express";
 const app = express()
 const port = 3000
 
+// set ejs as view engine
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res) => {
-    res.send(`Testing express ${port}`)
+    res.render("index",{App:'Cine Friend'})
 })
 app.listen(port, ()=>{
     console.log(`App live at http://localhost:${3000}`)
