02283cd91798ea8cfad89bcd7c4fb7039ab4777f
s-afrid <afrid0052@gmail.com>
2025-09-29 08:48:44 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express()
 const port = 3000
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // set ejs as view engine
 app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+// include static files
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req,res) => {
     res.render("index",{App:'Cine Friend'})
