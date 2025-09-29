44b3e7de04600f4980354315f638a24ab75827e1
s-afrid <afrid0052@gmail.com>
2025-09-29 11:10:33 +0530

    Add JavaScript functionality to send movie name for analysis
    - Implemented event listener on the "analyse" button to capture the movie name input.
    - Added asynchronous fetch request to send the movie name to the server.
    - Initialized the script with a main function to set up event handling.

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ app.set('view engine', 'ejs')
 app.set("views", path.join(__dirname, "views"));
 // include static files
 app.use(express.static(path.join(__dirname, "public")));
+// Parse text and json
+app.use(express.text())
 
 app.get('/', (req,res) => {
     res.render("index",{App:'Cine Friend'})
 })
+app.post('/',(req,res)=>{
+    const name = req.body;
+    console.log(name)
+})
+
 app.listen(port, ()=>{
     console.log(`App live at http://localhost:${3000}`)
 })
---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+let movie_name = document.getElementById("movie_name")
+let analyse = document.getElementById("analyse")
+
+
+function sendName() {
+    analyse.addEventListener("click",async (e)=>{
+        let a = await fetch('/',{method: "POST",
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            body: movie_name.value
+        })
+        
+    })
+}
+
+async function main() {
+    sendName();
+}
+
+main()
